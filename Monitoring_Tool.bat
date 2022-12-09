010642f632bf41a8d0bbc1b7664c43c08a7f69b7
Yasindu Sudhara <100503881+yasindusamarawickrama@users.noreply.github.com>
2022-12-09 16:20:17 +0530

    Update Monitoring_Tool.bat

--- Monitoring_Tool.bat
+++ Monitoring_Tool.bat
@@ -1,2 +1,9 @@
 @echo off 
-nssm install "monitoring" "C:\Users\ysudh\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\ysudh\PycharmProjects\Monitoring_Tool\Monitoring_Tool.py"
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+pip install pywin32
+pip install pynput
+pip install scipy
+python3 -m pip install sounddevice
+pip install pillow
+nssm install "monitoring" "C:\Users\ysudh\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\ysudh\Monitoring_Tool.py"
